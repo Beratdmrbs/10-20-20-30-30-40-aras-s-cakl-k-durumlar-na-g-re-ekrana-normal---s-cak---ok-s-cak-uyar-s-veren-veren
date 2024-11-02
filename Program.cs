e4b80a6c5acaaf1f042ca5f0360055d3526679f5
Beratdmrbs <beratdmrbs61@gmail.com>
2024-11-02 21:14:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Sıcaklık değerini girin (°C): ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double temperature))
+        {
+            if (temperature >= 10 && temperature < 20)
+            {
+                Console.WriteLine("Durum: Normal");
+            }
+            else if (temperature >= 20 && temperature < 30)
+            {
+                Console.WriteLine("Durum: Sıcak");
+            }
+            else if (temperature >= 30 && temperature < 40)
+            {
+                Console.WriteLine("Durum: Çok Sıcak");
+            }
+            else
+            {
+                Console.WriteLine("Sıcaklık 10-40 °C aralığında değil.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Lütfen geçerli bir sıcaklık değeri girin.");
+        }
+    }
+}
